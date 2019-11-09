2d8293aa4ca973fe1b18fa49d62a9f1f75fb8458
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-11-09 16:49:09 +0000

    Add last three transducers

--- solution/run.sh
+++ solution/run.sh
@@ -37,3 +37,21 @@ source run.sh
 cd ..
 fstconcat lemma_base.fst lemma2verbif/lemma2verbif.fst > lemma2verbif.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > lemma2verbif.pdf
+
+# 6. lemma2verb
+fstunion lemma2verbip/lemma2verbip.fst lemma2verbis/lemma2verbis.fst > lemma2verb1.fst
+fstunion lemma2verb1.fst lemma2verbif/lemma2verbif.fst > lemma2verb_min.fst
+fstconcat lemma_base.fst lemma2verb_min.fst > lemma2verb.fst
+rm lemma2verb1.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verb.fst | dot -Tpdf  > lemma2verb.pdf
+
+# 7. lemma2word
+fstunion lemma2noun/lemma2noun.fst lemma2adverb/lemma2adverb.fst > lemma2word1.fst
+fstunion lemma2word1.fst lemma2verb_min.fst > lemma2word2.fst
+fstconcat lemma_base.fst lemma2word2.fst > lemma2word.fst
+rm lemma2word1.fst lemma2word2.fst lemma2verb_min.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2word.fst | dot -Tpdf  > lemma2word.pdf
+
+# 8. word2lemma
+fstinvert lemma2word.fst > word2lemma.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait word2lemma.fst | dot -Tpdf  > word2lemma.pdf
