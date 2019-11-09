8bccb8913424357d48df186aa09abca55ab74964
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-11-09 17:02:50 +0000

    Prepare final deliver

--- solution/run.sh
+++ solution/run.sh
@@ -1,6 +1,7 @@
+# Base transducer
 fstcompile --isymbols=syms.txt --osymbols=syms.txt lemma_base.txt | fstarcsort > lemma_base.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma_base.fst | dot -Tpdf  > lemma_base.pdf
 
+# a.
 # 1. lemma2noun
 cd lemma2noun
 source run.sh
@@ -38,6 +39,7 @@ cd ..
 fstconcat lemma_base.fst lemma2verbif/lemma2verbif.fst > lemma2verbif.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > lemma2verbif.pdf
 
+# b.
 # 6. lemma2verb
 fstunion lemma2verbip/lemma2verbip.fst lemma2verbis/lemma2verbis.fst > lemma2verb1.fst
 fstunion lemma2verb1.fst lemma2verbif/lemma2verbif.fst > lemma2verb_min.fst
@@ -52,6 +54,21 @@ fstconcat lemma_base.fst lemma2word2.fst > lemma2word.fst
 rm lemma2word1.fst lemma2word2.fst lemma2verb_min.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2word.fst | dot -Tpdf  > lemma2word.pdf
 
+# c.
 # 8. word2lemma
 fstinvert lemma2word.fst > word2lemma.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait word2lemma.fst | dot -Tpdf  > word2lemma.pdf
+
+# d.
+# Test transducers
+# TODO
+
+# Final deliver
+mkdir -p FINALtransducers
+mv lemma2noun.fst lemma2adverb.fst lemma2verbip.fst lemma2verbis.fst lemma2verbif.fst lemma2verb.fst lemma2word.fst word2lemma.fst FINALtransducers
+
+mkdir -p FINALpdf
+mv lemma2noun.pdf lemma2adverb.pdf lemma2verbip.pdf lemma2verbis.pdf lemma2verbif.pdf lemma2verb.pdf lemma2word.pdf word2lemma.pdf FINALpdf
+
+mkdir -p FINALexamples
+# TODO
