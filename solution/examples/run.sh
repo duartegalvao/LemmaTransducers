7d44a283628e660ba8da4e63f1667dad6917e80c
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-11-15 17:58:19 +0000

    Simplify and finish tests

--- solution/examples/run.sh
+++ solution/examples/run.sh
@@ -1,53 +1,119 @@
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt objecto.txt | fstarcsort > objecto.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait objecto.fst | dot -Tpdf  > objecto.pdf
+# acabarei
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt acabarei.txt | fstarcsort > test1.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test1.fst | dot -Tpdf  > test1.pdf
 
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt acabarei.txt | fstarcsort > acabarei.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait acabarei.fst | dot -Tpdf  > acabarei.pdf
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt acabarei_lemma.txt | fstarcsort > test2.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test2.fst | dot -Tpdf  > test2.pdf
 
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt obviamente.txt | fstarcsort > obviamente.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait obviamente.fst | dot -Tpdf  > obviamente.pdf
+# aluna
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt aluna.txt | fstarcsort > test3.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test3.fst | dot -Tpdf  > test3.pdf
 
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt aluna_n_lemma.txt | fstarcsort > test4.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test4.fst | dot -Tpdf  > test4.pdf
 
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt objecto_lemma.txt | fstarcsort > objecto_lemma.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait objecto_lemma.fst | dot -Tpdf  > objecto_lemma.pdf
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt aluna_v_lemma.txt | fstarcsort > test5.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test5.fst | dot -Tpdf  > test5.pdf
 
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt acabarei_lemma.txt | fstarcsort > acabarei_lemma.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait acabarei_lemma.fst | dot -Tpdf  > acabarei_lemma.pdf
+# alunas
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt alunas.txt | fstarcsort > test6.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test6.fst | dot -Tpdf  > test6.pdf
 
-fstcompile --isymbols=../syms.txt --osymbols=../syms.txt obviamente_lemma.txt | fstarcsort > obviamente_lemma.fst
-fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait obviamente_lemma.fst | dot -Tpdf  > obviamente_lemma.pdf
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt alunas_n_lemma.txt | fstarcsort > test7.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test7.fst | dot -Tpdf  > test7.pdf
 
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt alunas_v_lemma.txt | fstarcsort > test8.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test8.fst | dot -Tpdf  > test8.pdf
+
+# concatenámos
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt concatenámos.txt | fstarcsort > test9.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test9.fst | dot -Tpdf  > test9.pdf
+
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt concatenámos_lemma.txt | fstarcsort > test10.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test10.fst | dot -Tpdf  > test10.pdf
+
+# sinceramente
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt sinceramente.txt | fstarcsort > test11.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test11.fst | dot -Tpdf  > test11.pdf
+
+fstcompile --isymbols=../syms.txt --osymbols=../syms.txt sinceramente_lemma.txt | fstarcsort > test12.fst
+fstdraw    --isymbols=../syms.txt --osymbols=../syms.txt --portrait test12.fst | dot -Tpdf  > test12.pdf
+
+### Begin tests
 
 # Testing lemma2noun
+fstcompose test4.fst ../lemma2noun.fst > test4_lemma2noun.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test4_lemma2noun.fst  |  dot -Tpdf  > test4_lemma2noun.pdf
+
+fstcompose test7.fst ../lemma2noun.fst > test7_lemma2noun.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test7_lemma2noun.fst  |  dot -Tpdf  > test7_lemma2noun.pdf
 
 # Testing lemma2adverb
+fstcompose test12.fst ../lemma2adverb.fst > test12_lemma2adverb.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test12_lemma2adverb.fst  |  dot -Tpdf  > test12_lemma2adverb.pdf
 
 # Testing lemma2verbip
+fstcompose test5.fst ../lemma2verbip.fst > test5_lemma2verbip.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test5_lemma2verbip.fst  |  dot -Tpdf  > test5_lemma2verbip.pdf
+
+fstcompose test8.fst ../lemma2verbip.fst > test8_lemma2verbip.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test8_lemma2verbip.fst  |  dot -Tpdf  > test8_lemma2verbip.pdf
 
 # Testing lemma2verbis
+fstcompose test10.fst ../lemma2verbis.fst > test10_lemma2verbis.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test10_lemma2verbis.fst  |  dot -Tpdf  > test10_lemma2verbis.pdf
 
 # Testing lemma2verbif
+fstcompose test2.fst ../lemma2verbif.fst > test2_lemma2verbif.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test2_lemma2verbif.fst  |  dot -Tpdf  > test2_lemma2verbif.pdf
 
 # Testing lemma2verb
-fstcompose acabarei_lemma.fst ../lemma2verb.fst > acabarei_lemma2verb.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait acabarei_lemma2verb.fst  |  dot -Tpdf  > acabarei_lemma2verb.pdf
+fstcompose test2.fst ../lemma2verb.fst > test2_lemma2verb.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test2_lemma2verb.fst  |  dot -Tpdf  > test2_lemma2verb.pdf
+
+fstcompose test5.fst ../lemma2verb.fst > test5_lemma2verb.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test5_lemma2verb.fst  |  dot -Tpdf  > test5_lemma2verb.pdf
+
+fstcompose test8.fst ../lemma2verb.fst > test8_lemma2verb.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test8_lemma2verb.fst  |  dot -Tpdf  > test8_lemma2verb.pdf
+
+fstcompose test10.fst ../lemma2verb.fst > test10_lemma2verb.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test10_lemma2verb.fst  |  dot -Tpdf  > test10_lemma2verb.pdf
 
 # Testing lemma2word
-fstcompose objecto_lemma.fst ../lemma2word.fst > objecto_lemma2word.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait objecto_lemma2word.fst  |  dot -Tpdf  > obviamente_lemma2word.pdf
+fstcompose test2.fst ../lemma2word.fst > test2_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test2_lemma2word.fst  |  dot -Tpdf  > test2_lemma2word.pdf
 
-fstcompose acabarei_lemma.fst ../lemma2word.fst > acabarei_lemma2word.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait acabarei_lemma2word.fst  |  dot -Tpdf  > acabar_lemma2word.pdf
+fstcompose test4.fst ../lemma2word.fst > test4_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test4_lemma2word.fst  |  dot -Tpdf  > test4_lemma2word.pdf
 
-fstcompose obviamente_lemma.fst ../lemma2word.fst > obviamente_lemma2word.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait obviamente_lemma2word.fst  |  dot -Tpdf  > objecto_lemma2word.pdf
+fstcompose test5.fst ../lemma2word.fst > test5_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test5_lemma2word.fst  |  dot -Tpdf  > test5_lemma2word.pdf
+
+fstcompose test7.fst ../lemma2word.fst > test7_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test7_lemma2word.fst  |  dot -Tpdf  > test7_lemma2word.pdf
+
+fstcompose test8.fst ../lemma2word.fst > test8_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test8_lemma2word.fst  |  dot -Tpdf  > test8_lemma2word.pdf
+
+fstcompose test10.fst ../lemma2word.fst > test10_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test10_lemma2word.fst  |  dot -Tpdf  > test10_lemma2word.pdf
+
+fstcompose test12.fst ../lemma2word.fst > test12_lemma2word.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test12_lemma2word.fst  |  dot -Tpdf  > test12_lemma2word.pdf
 
 # Testing word2lemma
-fstcompose objecto.fst ../word2lemma.fst > objecto_word2lemma.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait objecto_word2lemma.fst | dot -Tpdf  > objecto_word2lemma.pdf
+fstcompose test1.fst ../word2lemma.fst > test1_word2lemma.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test1_word2lemma.fst  |  dot -Tpdf  > test1_word2lemma.pdf
 
-fstcompose acabarei.fst ../word2lemma.fst > acabar_word2lemma.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait acabar_word2lemma.fst | dot -Tpdf  > acabar_word2lemma.pdf
+fstcompose test3.fst ../word2lemma.fst > test3_word2lemma.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test3_word2lemma.fst  |  dot -Tpdf  > test3_word2lemma.pdf
 
-fstcompose obviamente.fst ../word2lemma.fst > obviamente_word2lemma.fst
-fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait obviamente_word2lemma.fst | dot -Tpdf  > obviamente_word2lemma.pdf
+fstcompose test6.fst ../word2lemma.fst > test6_word2lemma.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test3_word2lemma.fst  |  dot -Tpdf  > test3_word2lemma.pdf
+
+fstcompose test9.fst ../word2lemma.fst > test9_word2lemma.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test9_word2lemma.fst  |  dot -Tpdf  > test9_word2lemma.pdf
+
+fstcompose test11.fst ../word2lemma.fst > test11_word2lemma.fst
+fstdraw --isymbols=../syms.txt --osymbols=../syms.txt --portrait test11_word2lemma.fst  |  dot -Tpdf  > test11_word2lemma.pdf
